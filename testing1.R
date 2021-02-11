81716f77af5af0b552b5edc4e65969ae4736576e
Beanalini <dat826@gmail.com>
2021-02-11 14:33:11 +0000

    second commit

--- testing1.R
+++ testing1.R
@@ -1,3 +1,5 @@
 print("Testing the link between Rstudio and GitHub for the first time")
 
-print("Let us se if it works okay this time!?")
+print("Let us see if it works okay this time!?")
+
+print("Fingers crosssed for the second time")
